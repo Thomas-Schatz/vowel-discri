261419b0913687c8afd4dff6891bbdb81bab0575
Thomas Schatz <thomas.schatz@laposte.net>
2017-06-20 10:29:30 +0200

    Example code for fetching from OSF repo

--- osf_fetch_example.py
+++ osf_fetch_example.py
@@ -1,9 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun  20 10:00:10 2017
+
+@author: Thomas Schatz
+
+Example code for fetching data from our OSF repository.
+This just prints a list of the elements in the /Stimuli/
+folder of our osfstorage component.
+"""
+
 import jsonapi_requests
 import os.path as p
-import .osf_auth
+from _osf_auth import osf_username, osf_password
 
-api = jsonapi_requests.Api.config({ 'API_ROOT': 'https://api.osf.io/v2/', 'AUTH':(osf_username, osf_password),
-TIMEOUT: 20})
+
+api = jsonapi_requests.Api.config({ 'API_ROOT': 'https://api.osf.io/v2/',
+                                    'AUTH':(osf_username, osf_password),
+                                    'TIMEOUT': 20})
 storage_root = "https://api.osf.io/v2/nodes/zdtk7/files/osfstorage"
 endpoint = api.endpoint(storage_root)
 response = endpoint.get()
